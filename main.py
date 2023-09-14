14905a946f87345b1d582479fe9dd535c3e9b37b
Gabriela <gabrielaap.nunez@gmail.com>
2023-09-14 17:10:51 -0300

    Adicionadas linhas de código com a construção do widget QFrame para o menu lateral

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from PySide6.QtWidgets import QMainWindow, QApplication, QFrame, QGridLayout
+from PySide6.QtWidgets import QMainWindow, QApplication, QFrame, QGridLayout, QVBoxLayout
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Interface")
+        self.setWindowTitle("Interface pra Gabriela se localizar")
 
         # Criação das estruturas básicas
 
@@ -12,6 +12,16 @@ class MainWindow(QMainWindow):
         self.widget_principal.setLayout(self.layout_principal) # Setando Layout para o Widget principal
 
         self.setCentralWidget(self.widget_principal) # Tornando o widget principal como widget central da QMainWindow
+
+        # Criação de estruturas para aportar elementos visuais
+        # Cada novo widget secundário é adicionado ao QGridLayout que é o layout principal
+        # Os Widgets terciários (botões, por exemplo) são adicionados ao layout do widget secundário
+
+        self.menu_lateral = QFrame() # QFrame para o menu lateral que possui botões estratégicos
+        self.layout_menu_lateral = QVBoxLayout()
+        self.menu_lateral.setLayout(self.layout_menu_lateral)
+
+        self.layout_principal.addWidget(self.menu_lateral, 0, 0, 3, 1)
 
 
 if __name__ == "__main__":
